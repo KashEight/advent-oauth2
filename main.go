52ff321ab3c6690c6aa62d5aa70cafd2f7b5ec4d
kash <kashiwa.electory@gmail.com>
2020-12-17 21:52:24 +0900

    chore: show localhost url

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -8,6 +9,8 @@ import (
 func main() {
 	http.HandleFunc("/", handlerSignIn)
 	http.HandleFunc("/callback", handlerCallback)
+
+	fmt.Println("Listen here: http://localhost:8080")
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
