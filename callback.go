da5e5e113aeed53e0dc3ed78a02944d9caca0f6f
kash <kashiwa.electory@gmail.com>
2020-12-23 10:08:42 +0900

    refactor: change to library-based token-exchange method

--- callback.go
+++ callback.go
@@ -1,12 +1,11 @@
 package main
 
 import (
+	"context"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
-	"net/url"
 	"strings"
 )
 
@@ -26,12 +25,17 @@ func handlerCallback(w http.ResponseWriter, r *http.Request) {
 
 	code := queries.Get("code")
 
-	idToken, err := getToken(code)
+	token, err := AuthConfig.Exchange(context.Background(), code)
 
 	if err != nil {
 		fmt.Fprintf(w, "%s", err)
 		return
 	}
+
+	idToken := token.Extra("id_token").(string)
+
+	fmt.Println("------ ID Token ------")
+	fmt.Println(idToken)
 
 	email, err := parseJWT(idToken)
 
@@ -41,57 +45,6 @@ func handlerCallback(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "email: %s", email)
-}
-
-const authUrl = "https://oauth2.googleapis.com/token"
-
-type authCallback struct {
-	IdToken string `json:"id_token"`
-}
-
-func getToken(code string) (string, error) {
-	values := url.Values{}
-
-	values.Add("code", code)
-	values.Add("client_id", AuthConfig.ClientID)
-	values.Add("client_secret", AuthConfig.ClientSecret)
-	values.Add("redirect_uri", AuthConfig.RedirectURL)
-	values.Add("grant_type", "authorization_code")
-
-	req, err := http.NewRequest("POST", authUrl, strings.NewReader(values.Encode()))
-
-	if err != nil {
-		return "", fmt.Errorf("failed building new request")
-	}
-
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-	client := new(http.Client)
-
-	resp, err := client.Do(req)
-
-	if err != nil {
-		return "", fmt.Errorf("failed request")
-	}
-
-	defer resp.Body.Close()
-
-	b, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-		return "", fmt.Errorf("failed reading body")
-	}
-
-	fmt.Println("------ Callback JSON Data ------")
-	fmt.Println(string(b))
-
-	clb := &authCallback{}
-
-	if err := json.Unmarshal(b, clb); err != nil {
-		return "", fmt.Errorf("failed unmarshal json data (in getToken())")
-	}
-
-	return clb.IdToken, nil
 }
 
 type jwtData struct {
